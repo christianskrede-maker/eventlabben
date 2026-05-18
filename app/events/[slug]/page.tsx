3c46f5b1640e52dda7acb11934390f17daf4e837
christianskrede-maker <christianskrede@gmail.com>
2026-05-18 15:00:04 +0200

    Update page.tsx

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -264,7 +264,7 @@ export default function EventPage({
           </p>
 
           <div className="badges">
-            <div className="badge">22. juni 2026</div>
+            <div className="badge">18. juni 2026</div>
             <div className="badge">Oslo</div>
             <div className="badge">Dresscode: fotballdrakt</div>
           </div>
