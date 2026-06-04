39ff29badb9757cd179893047050f336aaa3364b
christianskrede-maker <christianskrede@gmail.com>
2026-06-04 12:14:38 +0200

    Update foodtruck.ts

--- app/events/borgen-barne-og-gjenbruksfestival/data/foodtruck.ts
+++ app/events/borgen-barne-og-gjenbruksfestival/data/foodtruck.ts
@@ -1,1 +1,7 @@
-
+export const foodtruck = {
+  name: 'Kommer snart',
+  description:
+    'Her legger vi inn navn, meny, åpningstider og informasjon om foodtrucken når dette er klart.',
+  image:
+    '/events/borgen-barne-og-gjenbruksfestival/foodtruck.png',
+};
