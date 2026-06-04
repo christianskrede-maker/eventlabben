cf983b411a7693b05ec5f1f688d784ae8cf2c817
christianskrede-maker <christianskrede@gmail.com>
2026-06-04 12:14:08 +0200

    Update program.ts

--- app/events/borgen-barne-og-gjenbruksfestival/data/program.ts
+++ app/events/borgen-barne-og-gjenbruksfestival/data/program.ts
@@ -1,1 +1,18 @@
-
+export const program = [
+  {
+    time: '11:00',
+    title: 'Festivalområdet åpner',
+  },
+  {
+    time: 'Hele dagen',
+    title: 'DJ, musikk, bruktmarked, barneaktiviteter, stands, miljøaktiviteter og grilling',
+  },
+  {
+    time: 'Kommer',
+    title: 'Kulturinnslag og gjesteopptredener',
+  },
+  {
+    time: '18:00',
+    title: 'Festivalen avsluttes',
+  },
+];
