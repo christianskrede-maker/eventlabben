31bf776f227016bc46d8323f1b57dc86384e67b8
christianskrede-maker <christianskrede@gmail.com>
2026-06-04 12:15:02 +0200

    Update stands.ts

--- app/events/borgen-barne-og-gjenbruksfestival/data/stands.ts
+++ app/events/borgen-barne-og-gjenbruksfestival/data/stands.ts
@@ -1,1 +1,22 @@
-
+export const stands = [
+  {
+    id: 1,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 2,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 3,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 4,
+    title: 'Ledig',
+    description: '',
+  },
+];
