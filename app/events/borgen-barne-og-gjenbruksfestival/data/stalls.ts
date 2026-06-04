560582cb8f6062c0c9880e80fdd6e1f883b2b29d
christianskrede-maker <christianskrede@gmail.com>
2026-06-04 12:08:30 +0200

    Update stalls.ts

--- app/events/borgen-barne-og-gjenbruksfestival/data/stalls.ts
+++ app/events/borgen-barne-og-gjenbruksfestival/data/stalls.ts
@@ -1,1 +1,122 @@
-
+export const stalls = [
+  {
+    id: 1,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 2,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 3,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 4,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 5,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 6,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 7,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 8,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 9,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 10,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 11,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 12,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 13,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 14,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 15,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 16,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 17,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 18,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 19,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 20,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 21,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 22,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 23,
+    title: 'Ledig',
+    description: '',
+  },
+  {
+    id: 24,
+    title: 'Ledig',
+    description: '',
+  },
+];
